84361252914e399da9e5426db97e9667dce0ec7d
CaKlan <lhs74187@gmail.com>
2023-04-15 22:16:19 +0900

    main release

--- test.bat
+++ test.bat
@@ -1,9 +1,9 @@
-git branch main && \
-git checkout main && \
-git pull origin main && \
-git add . && \
-git commit -m "main release" && \
-git push origin main && \
-mdbook build && \
+git branch main | ^
+git checkout main | ^
+git pull origin main && ^
+git add . && ^
+git commit -m "main release" && ^
+git push origin main && ^
+mdbook build && ^
 call ./algorithms/book/release.bat
 
