827b7995d59f649fb5f2727796538d96c7427690
CaKlan <lhs74187@gmail.com>
2023-04-15 22:24:56 +0900

    main release

--- test.bat
+++ test.bat
@@ -4,5 +4,5 @@ git pull origin main && ^
 git add . && ^
 git commit -m "main release" && ^
 git push origin main && ^
-echo %~dp0algorithms/book/release.bat
+echo %~dp0algorithms\book\release.bat
 
